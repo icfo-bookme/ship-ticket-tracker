1e00a4557b7f2dec8a79684fb595015b0b1280f0
icfo-bookem <nationalambulancebd@gmail.com>
2026-08-13 17:43:59 +0600

    added

--- resources/views/refunded/refunded.blade.php
+++ resources/views/refunded/refunded.blade.php
@@ -1,6 +1,6 @@
 
 <div class="py-6">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+    <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 ">
         <div id="statusFilter" data-status="{{ "refunded" }}" class="hidden"></div>
         <h2 class="font-semibold text-xl text-gray-800  leading-tight">
             Refunded Ship Ticket Sales
--- resources/views/ship_ticket_sales/sales.blade.php
+++ resources/views/ship_ticket_sales/sales.blade.php
@@ -1,4 +1,4 @@
-     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+     <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 ">
          <div id="statusFilter" data-status="{{ $status }}" class="hidden"></div>
          <div class="flex items-center justify-between py-6">
              <h2 class="font-semibold text-xl text-gray-800  leading-tight">
